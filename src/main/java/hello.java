9709a9b3564b242060fe2f3d216ebe60b7904381
ding.cao <2271313916@qq.com>
2016-12-03 12:15:14 +0800

    this is my first program!

---
+++ src/main/java/hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created by txwhd on 2016/12/3.
+ */
+public class hello {
+    private String name="caoding";
+
+    public static void main(String[] args) {
+        hello hello = new hello();
+        System.out.println("hello world"+hello.name);
+    }
+}
